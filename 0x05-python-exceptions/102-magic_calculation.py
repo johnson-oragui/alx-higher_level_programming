bf09e517f08e3402f7c59c691c5b1468e1b531a9
sentense1 <3xsaint@gmail.com>
2023-03-29 02:34:50 -0700

    102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
+    '''Performs calculations between two integers
+
+    Args:
+        a: The first integer
+        b: The second integer
+
+    Returns:
+        If an error occurs - addition of integers
+        otherwise - the desired result
+    '''
     result = 0
     for i in range(1, 3):
         try:
