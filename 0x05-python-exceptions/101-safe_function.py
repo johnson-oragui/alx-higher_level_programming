4df7b832893b10853fc7b745c6e59cb65af3f139
sentense1 <3xsaint@gmail.com>
2023-03-30 00:17:46 -0700

    101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -18,5 +18,5 @@ def safe_function(fct, *args):
         result = fct(*args)
         return (result)
     except Exception:
-        print("{}".format(sys.exc_info()[1]))
+        print("{}".format(sys.exc_info()[1])), file=sys.stderr
         return (None)
