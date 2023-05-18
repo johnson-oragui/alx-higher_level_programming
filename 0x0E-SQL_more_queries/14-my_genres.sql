8adc40a057aec220805b5539ce0cf911ce1bcf80
sentense1 <3xsaint@gmail.com>
2023-05-18 06:58:04 -0700

    corretion

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,10 @@
 -- Write a script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
--- Query to select genres of the show Dexter
-SELECT tv_genres.name
-FROM hbtn_0d_tvshows.tv_genres
-JOIN hbtn_0d_tvshows.tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN hbtn_0d_tvshows.tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title = 'Dexter'
-ORDER BY tv_genres.name ASC;
+SELECT g.`name`
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS s
+       ON g.`id` = s.`genre_id`
+
+       INNER JOIN `tv_shows` AS t
+       ON t.`id` = s.`show_id`
+       WHERE t.`title` = "Dexter"
+ ORDER BY g.`name`;
