cc87bb673e0043df9f8e6b20dfd19f3e4747c298
sentense1 <3xsaint@gmail.com>
2023-05-18 06:52:22 -0700

    correction

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -6,4 +6,3 @@ JOIN hbtn_0d_tvshows.tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN hbtn_0d_tvshows.tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name ASC;
-
--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,8 +1,10 @@
 -- Lists all Comedy shows in the database hbtn_0d_tvshows.
-SELECT tv_shows.title
-FROM hbtn_0d_tvshows.tv_shows
-JOIN hbtn_0d_tvshows.tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN hbtn_0d_tvshows.tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name = 'Comedy'
-ORDER BY tv_shows.title ASC;
+SELECT t.`title`
+  FROM `tv_shows` AS t
+       INNER JOIN `tv_show_genres` AS s
+       ON t.`id` = s.`show_id`
 
+       INNER JOIN `tv_genres` AS g
+       ON g.`id` = s.`genre_id`
+       WHERE g.`name` = "Comedy"
+ ORDER BY t.`title`;
