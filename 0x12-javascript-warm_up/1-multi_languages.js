1d75b2e092760be0a68c6be3716deb82f277b1b9
sentense1 <3xsaint@gmail.com>
2023-06-14 13:41:02 -0700

    correction

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const firstLine = 'C is fun';
-const secondLine = 'Python is coo';
+const secondLine = 'Python is cool';
 const thirdLine = 'JavaScript is amazing';
 console.log(firstLine);
 console.log(secondLine);
