fd5762597e9893ecd50392998ee56d48f760c17b
sentense1 <3xsaint@gmail.com>
2023-06-14 01:22:14 -0700

    1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+let firstLine = 'C is fun';
+let secondLine = 'Python is coo';
+let thirdLine = 'JavaScript is amazing';
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);
