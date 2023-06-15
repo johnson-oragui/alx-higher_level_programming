60b88ef052fe8de21066f7f34e02c36be5681fc2
sentense1 <3xsaint@gmail.com>
2023-06-15 09:24:13 -0700

    task

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+//using the class notation for defining class
+class Rectangle {
+  //The constructor take 2 arguments: w and h
+  constructor (w, h) {
+    //If w or h is equal to 0 or not a positive integer, create an empty object
+    if (w > 0 && h > 0) {
+      //Initializing the instance attribute width with the value of w
+      //Initializing the instance attribute height with the value of h
+      [this.width, this.height] = [w, h];
+    }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    [this.width, this.height] = [this.width * 2, this.height * 2];
+  }
+};
+
+module.exports = Rectangle;
