af8f273010b7d4035b34ed65e30f6ac13c43ea31
sentense1 <3xsaint@gmail.com>
2023-06-17 01:04:12 -0700

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,36 @@
-#!/usr/bin/python3
-# Lists all states from the database hbtn_0e_0_usa.
-# Usage: ./0-select_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
+#!/usr/bin/env python3
+
 import sys
 import MySQLdb
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall()]
+def list_states(username, password, database):
+    # Connect to MySQL server
+    db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
+    
+    # Create a cursor object to execute SQL queries
+    cursor = db.cursor()
+    
+    # Execute the SQL query to retrieve states
+    cursor.execute('SELECT * FROM states ORDER BY id ASC')
+    
+    # Fetch all the rows from the result set
+    rows = cursor.fetchall()
+    
+    # Display the results
+    for row in rows:
+        print(row)
+    
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
+
+# Check if the script is being run directly
+if __name__ == '__main__':
+    # Get command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    
+    # Call the function to list states
+    list_states(username, password, database)
+
