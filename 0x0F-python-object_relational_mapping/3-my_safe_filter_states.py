d73cf7789e771c5d250a9d813dd760d542186fe2
sentense1 <3xsaint@gmail.com>
2023-06-17 04:35:23 -0700

    task

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,7 +10,41 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
+    import MySQLdb
+    import sys
+
+    # Get the command-line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to the MySQL server
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
+    )
+
+    # Create a cursor object to execute queries
+    cursor = db.cursor()
+
+    # Prepare the SQL query with placeholders
+    sql_query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+
+    # Execute the query with the state name as a parameter
+    cursor.execute(sql_query, (state_name,))
+
+    # Fetch all the rows returned by the query
+    rows = cursor.fetchall()
+
+    # Display the results
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
+
