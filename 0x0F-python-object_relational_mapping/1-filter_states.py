13f903259d051d0ebf0b5d046e2762da5ce0fa89
sentense1 <3xsaint@gmail.com>
2023-06-17 07:47:32 -0700

    task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,39 +1,52 @@
 #!/usr/bin/python3
-"""Module that lists all states from the hbtn_0e_0_usa database."""
+"""
+Module that lists all states starting with N from the hbtn_0e_0_usa database.
+"""
+
 import MySQLdb
 import sys
 
-# Get the command-line arguments
-mysql_username = sys.argv[1]
-mysql_password = sys.argv[2]
-database_name = sys.argv[3]
 
-# Connect to the MySQL server
-db = MySQLdb.connect(
-    host='localhost',
-    port=3306,
-    user=mysql_username,
-    passwd=mysql_password,
-    db=database_name
-)
+def list_states_with_N(username, password, database):
+    """Lists all states starting with N from the hbtn_0e_0_usa database.
+    
+    Args:
+        username: The username
+        password: The password
+        database: The database
+    """
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
 
-# Create a cursor object to execute queries
-cursor = db.cursor()
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
 
-# Prepare the SQL query
-sql_query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    # Execute the SQL query to retrieve states starting with N
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    cursor.execute(query)
 
-# Execute the query
-cursor.execute(sql_query)
+    # Fetch all the rows and print the state names
+    states = cursor.fetchall()
+    for state in states:
+        print(state[1])
 
-# Fetch all the rows returned by the query
-rows = cursor.fetchall()
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
 
-# Display the results
-for row in rows:
-    print(row)
 
-# Close the cursor and database connection
-cursor.close()
-db.close()
+if __name__ == "__main__":
+    # Get MySQL credentials from command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # List all states starting with N
+    list_states_with_N(username, password, database)
 
