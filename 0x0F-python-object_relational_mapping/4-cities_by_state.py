cae277905a4abb6884d92f08f08dcb1da4f97c2c
sentense1 <3xsaint@gmail.com>
2023-06-17 08:09:35 -0700

    task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-# Lists all cities of the database hbtn_0e_4_usa, ordered by city id.
-# Usage: ./4-cities_by_state.py <mysql username> \
-#                               <mysql password> \
-#                               <database name>
+"""Module that lists all states from the hbtn_0e_0_usa database."""
+
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
+    # Get MySQL credentials and search name from command-line arguments
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    # Connect to MySQL server
     c = db.cursor()
+
+     # Execute the SQL query to retrieve all states
     c.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
                  FROM `cities` as `c` \
                 INNER JOIN `states` as `s` \
                    ON `c`.`state_id` = `s`.`id` \
                 ORDER BY `c`.`id`")
+
+    # Fetch all rows and print the states
     [print(city) for city in c.fetchall()]
