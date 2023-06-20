79a8783cdda8eb5b71df3dec8cf56ef0e7d6e9e6
sentense1 <3xsaint@gmail.com>
2023-06-20 01:02:49 -0700

    correcting

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,6 +4,12 @@ import sys
 import MySQLdb
 
 def list_states (username, password, database):
+    """lists all states from the database hbtn_0e_0_usa.
+    Ags:
+        username: mysql username
+        password: mysql password
+        database: mysql database
+    """
     # Connect to the MySQL server
     db = MySQLdb.connect(host='localhost',\
             port=3306,\
