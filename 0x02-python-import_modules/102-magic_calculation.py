1a5d8ca7deb3188d182df3532c82a03fbd231eea
sentense1 <3xsaint@gmail.com>
2023-06-21 04:56:38 -0700

    module

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
     """Match the python bytecode."""
-    def magic_calculation(a, b):
     add = __import__('magic_calculation_102').add
     sub = __import__('magic_calculation_102').sub
 
