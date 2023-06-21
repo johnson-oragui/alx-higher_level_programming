d556056a293ea43343ed4d3d60f328d987134bf3
sentense1 <3xsaint@gmail.com>
2023-06-21 02:22:57 -0700

    t

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 
-def delete_at(my_list=[], idx=0):
+def delete_at(my_list=None, idx=0):
     """Delete an item at a specific position in a list."""
-    # Check if the index is negative or out of range
-    if idx < 0 or idx >= len(my_list):
-        return my_list.copy()
-
-    # Create a new list and copy the elements before the specified index
-    new_list = my_list[:idx]
-
-    # Copy the elements after the specified index
-    new_list.extend(my_list[idx + 1:])
-
-    # Return the new list with the item at the specified index removed
-    return new_list
+    if my_list is None:
+        # Set an empty list if my_list is None
+        my_list = []
+    
+    if 0 <= idx < len(my_list):
+        # Remove the item at the specified index using the del statement
+        del my_list[idx]
+    # Return the modified list
+    return my_list
