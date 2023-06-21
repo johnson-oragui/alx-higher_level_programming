f2d7bd6b1d953c8955d93673a84442e6f3898206
sentense1 <3xsaint@gmail.com>
2023-06-21 02:24:17 -0700

    t

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -5,7 +5,7 @@ def delete_at(my_list=None, idx=0):
     if my_list is None:
         # Set an empty list if my_list is None
         my_list = []
-    
+
     if 0 <= idx < len(my_list):
         # Remove the item at the specified index using the del statement
         del my_list[idx]
