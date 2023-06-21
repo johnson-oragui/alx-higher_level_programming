6e24bbffefda9ca63c847de8fcdd684c9f6ce957
sentense1 <3xsaint@gmail.com>
2023-06-21 01:15:41 -0700

    pycodestyle

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
-# 11-delete_at.py
-# Brennan D Baraban <375@holbertonschool.com>
-
 
 def delete_at(my_list=[], idx=0):
     """Delete an item at a specific position in a list."""
