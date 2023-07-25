aad902f7fd19fed4060e6639f848cd8592c17955
sentense1 <3xsaint@gmail.com>
2023-07-25 14:29:15 -0700

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,30 +1,16 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-// Get the API URL from the first command-line argument
-const apiUrl = process.argv[2];
-
-// Use the 'request' module to perform an HTTP GET request
-request(apiUrl, function (error, response, body) {
-  // Check if there was no error during the HTTP request
-  if (!error && response.statusCode === 200) {
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
     const todos = JSON.parse(body);
-    // Create an empty object to store the count of completed tasks for each user ID.
-    const completedTasksByUserId = {};
-    // Iterate through each todo in the 'todos' array.
+    let completed = {};
     todos.forEach((todo) => {
-      if (todo.completed) {
-        if (completedTasksByUserId[todo.userId] === undefined) {
-          completedTasksByUserId[todo.userId] = 1;
-        } else {
-          completedTasksByUserId[todo.userId] += 1;
-        }
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
       }
     });
-
-    console.log(completedTasksByUserId);
-  } else {
-    console.error('Error:', error);
+    console.log(completed);
   }
 });
