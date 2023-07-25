94397c0528029276dbcf7fbc67ca0894aceed374
sentense1 <3xsaint@gmail.com>
2023-07-25 14:24:45 -0700

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,30 @@
 #!/usr/bin/node
 
-// Import the 'request' module
 const request = require('request');
 
-// Use the 'request' module to perform an HTTP GET request to the URL
-request(process.argv[2], function (error, response, body) {
-  // Check if there was no error during the HTTP request.
-  if (!error) {
-    // parse the JSON data using 'JSON.parse()'
+// Get the API URL from the first command-line argument
+const apiUrl = process.argv[2];
+
+// Use the 'request' module to perform an HTTP GET request
+request(apiUrl, function (error, response, body) {
+  // Check if there was no error during the HTTP request
+  if (!error && response.statusCode === 200) {
     const todos = JSON.parse(body);
-
-    // Create an empty object 'completed' to store the count of completed todos for each user ID.
-    const completed = {};
-
+    // Create an empty object to store the count of completed tasks for each user ID.
+    const completedTasksByUserId = {};
     // Iterate through each todo in the 'todos' array.
     todos.forEach((todo) => {
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else if (todo.completed) {
-        completed[todo.userId] += 1;
+      if (todo.completed) {
+        if (completedTasksByUserId[todo.userId] === undefined) {
+          completedTasksByUserId[todo.userId] = 1;
+        } else {
+          completedTasksByUserId[todo.userId] += 1;
+        }
       }
     });
-    console.log(completed);
+
+    console.log(completedTasksByUserId);
+  } else {
+    console.error('Error:', error);
   }
 });
