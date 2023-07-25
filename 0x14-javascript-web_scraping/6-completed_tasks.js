c8060c0d8fac187f66752312002d9ba51291ef98
sentense1 <3xsaint@gmail.com>
2023-07-25 14:38:09 -0700

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,27 @@
 #!/usr/bin/node
+
 const request = require('request');
+
 request(process.argv[2], function (error, response, body) {
-  if (!error) {
-    const todos = JSON.parse(body);
-    let completed = {};
-    todos.forEach((todo) => {
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else if (todo.completed) {
-        completed[todo.userId] += 1;
-      }
-    });
-    console.log(completed);
+  if (!error && response.statusCode === 200) {
+    try {
+      const todos = JSON.parse(body);
+
+      const completed = {};
+
+      todos.forEach((todo) => {
+        if (todo.completed && completed[todo.userId] === undefined) {
+          completed[todo.userId] = 1;
+        } else if (todo.completed) {
+          completed[todo.userId] += 1;
+        }
+      });
+
+      console.log(completed);
+    } catch (parseError) {
+      console.error('Error parsing JSON:', parseError);
+    }
+  } else {
+    console.error('Error:', error);
   }
 });
