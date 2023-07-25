fd45183cb7a719b23615b21ef850dd1a75831c05
sentense1 <3xsaint@gmail.com>
2023-07-25 14:54:18 -0700

    100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -6,19 +6,14 @@ const movieId = process.argv[2];
 const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
 request(apiUrl, function (error, response, body) {
-
   if (!error && response.statusCode === 200) {
-
     const movieData = JSON.parse(body);
 
     console.log(`Characters of "${movieData.title}":`);
 
     movieData.characters.forEach((characterUrl) => {
-
       request(characterUrl, function (charError, charResponse, charBody) {
-
         if (!charError && charResponse.statusCode === 200) {
-
           const characterData = JSON.parse(charBody);
 
           console.log(characterData.name);
@@ -27,7 +22,6 @@ request(apiUrl, function (error, response, body) {
         }
       });
     });
-
   } else {
     console.error('Error fetching movie data:', error);
   }
