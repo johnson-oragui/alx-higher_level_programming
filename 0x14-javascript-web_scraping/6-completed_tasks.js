88cdaa3f6890f71df9081d37eaabb97435683085
sentense1 <3xsaint@gmail.com>
2023-07-26 06:40:33 -0700

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,33 +2,32 @@
 
 const request = require('request');
 
-// Function to compute the number of completed tasks by user ID
-function countCompletedTasks (todos) {
-  const completedTasksByUser = {};
-
-  todos.forEach((todo) => {
-    if (todo.completed) {
-      const userId = todo.userId;
-      completedTasksByUser[userId] = (completedTasksByUser[userId] || 0) + 1;
-    }
-  });
-
-  return completedTasksByUser;
-}
-
-// Main function to fetch data and compute the completed tasks
-function fetchAndComputeCompletedTasks (apiUrl) {
-  request(apiUrl, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-      const todos = JSON.parse(body);
-      const completedTasksByUser = countCompletedTasks(todos);
-
-      console.log(completedTasksByUser);
-    } else {
-      console.error('Error fetching data:', error);
-    }
-  });
-}
-
 const apiUrl = process.argv[2];
-fetchAndComputeCompletedTasks(apiUrl);
+
+request(apiUrl, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    try {
+      const todos = JSON.parse(body);
+
+      const completed = {};
+
+      todos.forEach((todo) => {
+        if (todo.completed) {
+          if (completed[todo.userId] === undefined) {
+            completed[todo.userId] = 1;
+          } else {
+            completed[todo.userId]++;
+          }
+        }
+      });
+
+      const output = `{${Object.entries(completed).map(([key, value]) => ` '${key}': ${value}`).join(',\n ')} }`;
+
+      console.log(Object.keys(completed).length > 2 ? output : output.replace(/\n/g, ''));
+    } catch (parseError) {
+      console.error('Error parsing JSON:', parseError);
+    }
+  } else {
+    console.error('Error:', error);
+  }
+});
