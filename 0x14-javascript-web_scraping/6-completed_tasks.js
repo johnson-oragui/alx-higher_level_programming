40dc3cfaf01e022fc675cb649ff63323bcf33f31
sentense1 <3xsaint@gmail.com>
2023-07-26 02:13:03 -0700

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,23 +1,40 @@
 #!/usr/bin/node
-let request = require('request');
-let url = process.argv[2];
-let myDict = {};
 
-request(url, function (err, data, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    let response = JSON.parse(body);
+const request = require('request');
 
-    for (let i = 0; i < response.length; i++) {
-      if (response[i]['completed'] === true) {
-        if (myDict[response[i]['userId']] === undefined) {
-          myDict[response[i]['userId']] = 1;
-        } else {
-          myDict[response[i]['userId']] += 1;
+const apiUrl = process.argv[2];
+
+request(apiUrl, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    try {
+      const todos = JSON.parse(body);
+
+      const completed = {};
+
+      todos.forEach((todo) => {
+        if (todo.completed) {
+          if (completed[todo.userId] === undefined) {
+            completed[todo.userId] = 1;
+          } else {
+            completed[todo.userId]++;
+          }
         }
-      }
+      });
+
+      // Create the formatted output manually to match the expected format
+      let formattedOutput = '{\n';
+      Object.keys(completed).forEach((userId, index, keys) => {
+        formattedOutput += `  '${userId}': ${completed[userId]}`;
+        formattedOutput += index === keys.length - 1 ? '\n' : ',\n';
+      });
+      formattedOutput += '}\n';
+
+      console.log(formattedOutput);
+
+    } catch (parseError) {
+      console.error('Error parsing JSON:', parseError);
     }
+  } else {
+    console.error('Error fetching data:', error);
   }
-  console.log(myDict);
 });
