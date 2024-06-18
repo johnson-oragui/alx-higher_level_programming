2049206833d7e23482ba06a113193f949580a70e
johnson-oragui <johnson-oragui@gmail.com>
2024-06-18 20:20:34 +0100

    100-relationship_states_cities.py

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Module that adds a city and its associated state\
-        to a MySQL database using SQLAlchemy."""
+"""Module that adds a city and its associated state
+to a MySQL database using SQLAlchemy.
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -8,24 +9,20 @@ from relationship_state import State
 from relationship_city import Base, City
 
 
-# Create the SQLAlchemy engine using the provided MySQL credentials
-engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                       .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                       pool_pre_ping=True)
-# Create the database tables based on the defined models
-Base.metadata.create_all(engine)
+if __name__ == "__main__":
+    # Create the SQLAlchemy engine using the provided MySQL credentials
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    # Create the database tables based on the defined models
+    Base.metadata.create_all(engine)
 
-# Create a session factory
-Session = sessionmaker(bind=engine)
+    # Create a session factory
+    Session = sessionmaker(bind=engine)
 
-# Create a session object
-session = Session()
+    # Create a session object
+    session = Session()
 
-
-def create_state_city():
-    """
-    Creates state and city objects
-    """
     # Create a new city and its associated state
     state = State(name="California")
 
@@ -37,7 +34,3 @@ def create_state_city():
 
     # Commit the session to persist the changes in the database
     session.commit()
-
-
-if __name__ == "__main__":
-    create_state_city()
